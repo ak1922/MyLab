acb5432eb729f8113b94f811cdf3c328aa1399f0
ak1922 <116603167+ak1922@users.noreply.github.com>
2023-04-20 19:12:44 -0400

    boto3 list buckets

--- S3/list-bucket.py
+++ S3/list-bucket.py
@@ -4,5 +4,7 @@ import boto3
 s3 = boto3.resource('s3')
 
 # Print out all bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+def lambda_handler(event, context):
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+    return(bucket.name)
